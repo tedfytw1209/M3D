f91665896e57a4752adaaccbb39c58b9c9781b30
teddy <teddytw1209@gmail.com>
2025-02-06 22:53:37 -0500

    change dataset

---
+++ Data/process/m3d_ldct_data_prepare.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+import pandas as pd
+import nibabel as nib
+import json
+from PIL import Image
+import concurrent.futures
+from tqdm import tqdm
+from collections import Counter
+import unicodedata
+import monai.transforms as mtf
+from multiprocessing import Pool
+from unidecode import unidecode
+
+# input_dir = 'PATH/M3D_Cap/ct_quizze/'
+# output_dir = 'PATH/M3D_Cap_npy/ct_quizze/'
+
+input_dir = '/blue/bianjiang/tienyuchang/CT_nii/'
+output_dir = '/blue/bianjiang/tienyuchang/CT_npy/'
+
+# Get all subfolders [00001, 00002....]
+#subfolders = [folder for folder in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, folder))]
+
+
+transform = mtf.Compose([
+    mtf.CropForeground(),
+    mtf.Resize(spatial_size=[32, 256, 256], mode="bilinear")
+])
+
+
+def process_nii_files(nii_file):
+    output_id_folder = output_dir
+    input_id_folder = input_dir
+    nii_name = nii_file.replace('.nii', '')
+
+    os.makedirs(output_id_folder, exist_ok=True)
+
+    output_path = os.path.join(output_dir, f'{nii_name}.npy')
+
+    try:
+        final_3d_image = nib.load(os.path.join(input_id_folder,nii_file)).get_fdata()
+        image = final_3d_image[np.newaxis, ...]
+        image = image - image.min()
+        image = image / np.clip(image.max(), a_min=1e-8, a_max=None)
+        img_trans = transform(image)
+        np.save(output_path, img_trans)
+    except:
+        print(final_3d_image.shape)
+        print("This folder is vstack error: ", output_path)
+
+
+data_path = ""
+try:
+    with open(data_path) as fp:
+        list_data_dict = json.load(fp)
+except:
+    with open(data_path) as fp:
+        list_data_dict = [json.loads(q) for q in fp]
+nii_files = [data_dict['nii'] for data_dict in list_data_dict]
+
+with Pool(processes=32) as pool:
+    with tqdm(total=len(nii_files), desc="Processing") as pbar:
+        for _ in pool.imap_unordered(process_nii_files, nii_files):
+            pbar.update(1)
--- LaMed/src/dataset/multi_dataset.py
+++ LaMed/src/dataset/multi_dataset.py
@@ -1175,11 +1175,125 @@ class TextDatasets(Dataset):
     def __getitem__(self, idx):
         return self.dataset[idx]
 
+class LDCTNIIDataset(Dataset):
+    def __init__(self, args, tokenizer, mode="train"):
+        self.args = args
+        self.data_root = args.data_root
+        self.tokenizer = tokenizer
+        self.mode = mode
+
+        self.image_tokens = "<im_patch>" * args.proj_out_num
+
+        #with open(args.cap_data_path, 'r') as file:
+        #    self.json_file = json.load(file)
+        try:
+            with open(args.cap_data_path) as fp:
+                self.data_list = json.load(fp)
+        except:
+            with open(args.cap_data_path) as fp:
+                self.data_list = [json.loads(q) for q in fp]
+        #self.data_list = self.json_file[mode]
+
+        self.caption_prompts = Caption_templates
+
+        train_transform = mtf.Compose(
+            [
+                mtf.RandRotate90(prob=0.5, spatial_axes=(1, 2)),
+                mtf.RandFlip(prob=0.10, spatial_axis=0),
+                mtf.RandFlip(prob=0.10, spatial_axis=1),
+                mtf.RandFlip(prob=0.10, spatial_axis=2),
+                mtf.RandScaleIntensity(factors=0.1, prob=0.5),
+                mtf.RandShiftIntensity(offsets=0.1, prob=0.5),
+
+                mtf.ToTensor(dtype=torch.float),
+            ]
+        )
+
+        val_transform = mtf.Compose(
+                [
+                    mtf.ToTensor(dtype=torch.float),
+                ]
+            )
+        set_track_meta(False)
+
+        if mode == 'train':
+            self.transform = train_transform
+        elif mode == 'validation':
+            self.transform = val_transform
+        elif 'test' in mode:
+            self.transform = val_transform
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, idx):
+        max_attempts = 100
+        for _ in range(max_attempts):
+            try:
+                data = self.data_list[idx]
+                image_path = data["nii"]
+                image_path = image_path.replace('.nii', '.npy') #may need to change
+                image_abs_path = os.path.join(self.data_root, image_path)
+
+                image = np.load(image_abs_path)  # nomalized 0-1, C,D,H,W
+                # image = np.load(img_abs_path)[np.newaxis, ...]  # nomalized
+                image = self.transform(image)
+
+                answer = data["conversations"][-1]["value"]
+
+                prompt_question = random.choice(self.caption_prompts) # use caption prompts
+
+                question = self.image_tokens + prompt_question
+
+                text_tensor = self.tokenizer(
+                    question + ' ' + answer, max_length=self.args.max_length, truncation=True, padding="max_length", return_tensors="pt"
+                )
+
+                input_id = text_tensor["input_ids"][0]
+                attention_mask = text_tensor["attention_mask"][0]
+
+                valid_len = torch.sum(attention_mask)
+                if valid_len < len(input_id):
+                    input_id[valid_len] = self.tokenizer.eos_token_id
+
+                question_tensor = self.tokenizer(
+                    question, max_length=self.args.max_length, truncation=True, padding="max_length", return_tensors="pt"
+                )
+
+                question_len = torch.sum(question_tensor["attention_mask"][0])
+
+                label = input_id.clone()
+                label[:question_len] = -100
+                if self.tokenizer.pad_token_id == self.tokenizer.eos_token_id:
+                    label[label == self.tokenizer.pad_token_id] = -100
+                    if valid_len < len(label):
+                        label[valid_len] = self.tokenizer.eos_token_id
+                else:
+                    label[label == self.tokenizer.pad_token_id] = -100
+
+                ret = {
+                    'image': image,
+                    'input_id': input_id,
+                    'label': label,
+                    'attention_mask': attention_mask,
+                    'question': question,
+                    'answer': answer,
+                    'question_type': "Caption",
+                }
+                if self.args.seg_enable:
+                    ret.update({'seg': torch.zeros_like(image)})
+
+                return ret
+
+            except Exception as e:
+                print(f"Error in __getitem__ at index {idx}: {e}")
+                idx = random.randint(0, len(self.data_list) - 1)
+
 class CapDatasets(Dataset):
     def __init__(self, args, tokenizer, mode='train'):
         super(CapDatasets, self).__init__()
         self.ds_list = [
-            CapDataset(args, tokenizer, mode),
+            LDCTNIIDataset(args, tokenizer, mode),
         ]
         self.dataset = ConcatDataset(self.ds_list)
 
